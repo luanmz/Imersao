7343c3e66e391a0a87dc87f74743c15fa4ac0a74
Luan Menezes <luanpereiramz@gmail.com>
2022-01-10 20:14:52 -0300

    Update a,b

--- python.py
+++ python.py
@@ -5,4 +5,18 @@ print(a+b)
 def soma (num1,num2):
     return (num1+num2)
 
+<<<<<<< Updated upstream
 print(soma(12,b))
+=======
+print(soma(a,b))
+
+def sub (num1,num2):
+    return (num1-num2)
+
+print(sub(a,b))
+
+def mult (num1,num2):
+    return (num1*num2)
+
+print(mult(a,b))
+>>>>>>> Stashed changes
